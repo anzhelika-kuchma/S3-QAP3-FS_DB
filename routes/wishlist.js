e82482cdf47d4e4700708e134f32b8c16729d737
Anzhelika Kuchma <anzhelikakuchma@anzhelikas-air.home>
2024-03-27 20:26:45 -0230

    patch and delete are done and working fine

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -14,16 +14,34 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-    let aSong = await wishlistDal.getDestById(req.params.id);
-    if (aSong === undefined) {
+    let aDest = await wishlistDal.getDestById(req.params.id);
+    if (aDest === undefined) {
       res.render("norecord");
     } else {
       if (DEBUG) console.table(aDest);
-      res.render("destination", { aDest });
+      res.render("dest", { aDest });
     }
   } catch (err) {
     res.render("503");
   }
+});
+
+router.get("/:id/edit", async (req, res) => {
+  if (DEBUG) console.log("dest.Edit : " + req.params.id);
+  res.render("destPatch.ejs", {
+    destination: req.query.destination,
+    country: req.query.country,
+    city: req.query.city,
+  });
+});
+
+
+router.get("/:id/delete", async (req, res) => {
+  if (DEBUG) console.log("dest.Delete : " + req.params.id);
+  res.render("destDelete.ejs", {
+    destination: req.query.destination,
+    theId: req.params.id,
+  });
 });
 
 router.post("/", async (req, res) => {
@@ -31,12 +49,39 @@ router.post("/", async (req, res) => {
   try {
     await wishlistDal.addDest(
       req.body.destination,
-      req.body.city,
-      req.body.country
+      req.body.country,
+      req.body.city
     );
     res.redirect("/wishlist/");
   } catch (err) {
     // if (DEBUG) console.log(err);
+    res.render("503");
+  }
+});
+
+router.patch("/:id", async (req, res) => {
+  if (DEBUG) console.log("wishlist.PATCH: " + req.params.id);
+  try {
+    await wishlistDal.patchDest(
+      req.params.id,
+      req.body.destination,
+      req.body.country,
+      req.body.city
+    );
+    res.redirect("/wishlist/");
+  } catch {
+    // log this error to an error log file.
+    res.render("503");
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  if (DEBUG) console.log("wishlist.DELETE: " + req.params.id);
+  try {
+    await wishlistDal.deleteDest(req.params.id);
+    res.redirect("/wishlist/");
+  } catch {
+    // log this error to an error log file.
     res.render("503");
   }
 });
--- services/pg.wishlist.dal.js
+++ services/pg.wishlist.dal.js
@@ -1,10 +1,10 @@
 const dal = require("./pg.auth_db");
 
 var getDestinations = function () {
-  if (DEBUG) console.log("logins.pg.dal.getLogins()");
+  if (DEBUG) console.log("wishlist.pg.dal.getDestinations()");
   return new Promise(function (resolve, reject) {
-    const sql = `SELECT id, country, city, destination FROM public."Travel-Wishlist" \ 
-    ORDER BY id DESC;`;
+    const sql = `SELECT id, destination, country, city FROM public."Travel-Wishlist" \ 
+    ORDER BY id;`;
     dal.query(sql, [], (err, result) => {
       if (err) {
         if (DEBUG) console.log(err);
@@ -17,8 +17,8 @@ var getDestinations = function () {
 };
 
 async function getDestById(id) {
-  if (DEBUG) console.log("pg.wishlist.dal.getDestById()");
-  const sql = `SELECT id, destination, city, country FROM public."Travel-Wishlist" WHERE id = $1;`;
+  if (DEBUG) console.log("pg.destlist.dal.getDestById()");
+  const sql = `SELECT id, destination, country, city FROM public."Travel-Wishlist" WHERE id = $1;`;
   try {
     let result = await dal.query(sql, [id]);
     return result.rows[0];
@@ -28,12 +28,12 @@ async function getDestById(id) {
   }
 }
 
-async function addDest(destination, city, country) {
+async function addDest(destination, country, city) {
   if (DEBUG) console.log("pg.wishlist.dal.addDest()");
-  const sql = `INSERT INTO public."Travel-Wishlist"(destination, city, country) VALUES ($1, $2, $3) RETURNING id;`;
+  const sql = `INSERT INTO public."Travel-Wishlist"(destination, country, city) VALUES ($1, $2, $3, $4) RETURNING id;`;
   try {
-    let result = await dal.query(sql, [destination, city, country]);
-    if (DEBUG) console.log(result);
+    let result = await dal.query(sql, [destination, country, city]);
+    if (DEBUG) console.log(result.rows);
     return result.rows[0].id;
   } catch (error) {
     if (error.code === "23505") return error.code;
@@ -42,9 +42,36 @@ async function addDest(destination, city, country) {
   }
 }
 
+async function patchDest(id, destination, country, city) {
+  if (DEBUG) console.log("pg.wishlist.dal.patchDest()");
+  const sql = `UPDATE public."Travel-Wishlist" SET destination=$2, country=$3, city=$4 WHERE id=$1;`;
+  try {
+    let result = await dal.query(sql, [id, destination, country, city]);
+    if (DEBUG) console.log(result);
+    return result.rowCount;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+async function deleteDest(id) {
+  if (DEBUG) console.log("pg.wishlist.dal.deleteDest()");
+  const sql = `DELETE FROM public."Travel-Wishlist" WHERE id = $1;`;
+  try {
+    let result = await dal.query(sql, [id]);
+    if (DEBUG) console.log(result);
+    return result.rowCount;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
 
 module.exports = {
-    getDestinations,
-    getDestById,
-    addDest,
+  getDestinations,
+  addDest,
+  getDestById,
+  patchDest,
+  deleteDest,
 };
